bc9b2be651b5a11363347a76e10ea185044a41d3
Matthew Russell Downey <matthew.r.downey@gmail.com>
2017-04-30 22:37:35 -0400

    WIP: changing ArcEditor arc rendering

--- Assets/Code/Editor/ArcEditor.cs
+++ Assets/Code/Editor/ArcEditor.cs
@@ -18,20 +18,24 @@ public class ArcEditor : Editor
         Vector3 normal = arc.pole(radius);
         Vector3 center = Vector3.Project(from, normal);
 
-        DrawArc(from, to, center, color);
+        DrawArc(from, to, center, normal, color);
     }
 
-    // FIXME: 1) this fails when center is Vector3.zero 2) this does not work well when from = to = center for corners
-    void DrawArc(Vector3 from, Vector3 to, Vector3 center, Color color)
+    // FIXME: this doesn't quite work because arcs and their complementary arcs are ambiguous
+    void DrawArc(Vector3 from, Vector3 to, Vector3 center, Vector3 normal, Color color)
     {
+        if (from == center) // If this is a corner
+        {
+            return; // there is no need to render since corners have no radius
+        }
+
         float radius = (from - center).magnitude;
 
         from = (from - center).normalized;
         to = (to - center).normalized;
 
         Vector3 forward = from;
-        Vector3 up = center;
-        Vector3 right = -Vector3.Cross(from, up);
+        Vector3 right = -Vector3.Cross(forward, normal);
 
         float x = Vector3.Dot(to, forward);
         float y = Vector3.Dot(to, right);
@@ -39,12 +43,16 @@ public class ArcEditor : Editor
         float angle = Mathf.Atan2(y, x);
 
         UnityEditor.Handles.color = color;
-        UnityEditor.Handles.DrawWireArc(center, center, from, angle, radius);
+        UnityEditor.Handles.DrawWireArc(center, normal, from, angle, radius);
     }
    
     void DrawRadial(float angle, float radius, Color color)
     {
-        DrawArc(arc.position(angle, 0), arc.position(angle, radius), Vector3.zero, color); 
+        Vector3 from = arc.position(angle, 0);
+        Vector3 to = arc.position(angle, radius);
+        Vector3 normal = Vector3.Cross(from, to);
+
+        DrawArc(from, to, Vector3.zero, normal, color);
     }
 
     void OnSceneGUI()
@@ -57,3 +65,20 @@ public class ArcEditor : Editor
         DrawRadial(arc.angle(), 0.1f, Color.green);
     }
 }
+
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
