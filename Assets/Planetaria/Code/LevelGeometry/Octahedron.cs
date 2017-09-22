2debcdc59d9832f2de6c76e852bc7c113a4fff23
Matthew Russell Downey <matthew.r.downey@gmail.com>
2017-09-22 17:20:41 -0400

    Refactoring uv_to_cartesian and cartesian_to_uv

--- Assets/Planetaria/Code/LevelGeometry/Octahedron.cs
+++ Assets/Planetaria/Code/LevelGeometry/Octahedron.cs
@@ -34,36 +34,13 @@ public static class Octahedron
     /// <returns>The UV coordinates of an octahedron.</returns>
     public static Vector2 cartesian_to_uv(Vector3 octahedral)
     {
-        Vector2 uv = Vector2.zero;
-
         // get mapping index from 0-7
-        int triangle_start_index =
+        int xyz_mask =
                 (octahedral.x < 0 ? 1 : 0) + // x sign is 1s place
                 (octahedral.y < 0 ? 2 : 0) + // y sign is 2s place
                 (octahedral.z < 0 ? 4 : 0);  // z sign is 4s place
 
-        triangle_start_index *= 3; // there are 24 indices in the octahedron mesh (i.e. 3 triangle vertices times 8 faces)
-
-        Mesh mesh = octahedron_mesh();
-
-        for (int vertex = 0; vertex < 3; ++vertex)
-        {
-            int begin_edge = mesh.triangles[triangle_start_index + vertex];
-            int end_edge = mesh.triangles[triangle_start_index + (vertex + 1) % 3];
-
-            Vector2 begin_uv = mesh.uv[begin_edge];
-            Vector2 end_uv = mesh.uv[end_edge];
-
-            Vector3 begin_cartesian = mesh.vertices[begin_edge];
-            Vector3 end_cartesian = mesh.vertices[end_edge];
-
-            Vector3 edge_vector = (end_cartesian - begin_cartesian);
-            float dot_product = Vector2.Dot((octahedral - begin_cartesian), edge_vector) / edge_vector.sqrMagnitude;
-            Vector2 uv_vector = end_uv - begin_uv;
-            uv += uv_vector*dot_product;
-        }
-
-        return uv;
+        return convert<Vector3, Vector2>(octahedral, xyz_mask);
     }
 
     /// <summary>
@@ -73,15 +50,21 @@ public static class Octahedron
     /// <returns>The octahedral coordinates.</returns>
     public static Vector3 uv_to_cartesian(Vector2 uv)
     {
-        Vector3 cartesian = Vector3.zero;
-
         // get mapping index from 0-7
-        int triangle_start_index =
+        int xyz_mask =
                 (uv.x < 0.5f ? 1 : 0) + // x sign is 1s place
                 (Mathf.Abs(uv.x) + Mathf.Abs(uv.y) > 1 ? 2 : 0) + // y sign is 2s place
                 (uv.y < 0.5f ? 4 : 0);  // z sign is 4s place
 
-        triangle_start_index *= 3; // there are 24 indices in the octahedron mesh (i.e. 3 triangle vertices times 8 faces)
+        return convert<Vector2, Vector3>(uv, xyz_mask);
+    }
+
+    private static To convert<From, To>(From from, int xyz_mask) // FIXME: less than ideal, prefer using From[] To[] (supplied as parameters)
+    {
+        To to = (To) System.Activator.CreateInstance(typeof(To), new object[] {});
+
+        // get mapping index from 0-7
+        int triangle_start_index = xyz_mask * 3; // there are 24 indices in the octahedron mesh (i.e. 3 triangle vertices times 8 faces)
 
         Mesh mesh = octahedron_mesh();
 
@@ -96,16 +79,25 @@ public static class Octahedron
             Vector3 begin_cartesian = mesh.vertices[begin_edge];
             Vector3 end_cartesian = mesh.vertices[end_edge];
 
-            Vector2 edge_vector = (end_uv - begin_uv);
-            float dot_product = Vector2.Dot((uv - begin_uv), edge_vector) / edge_vector.sqrMagnitude;
+            Vector2 uv_vector = end_uv - begin_uv;
             Vector3 cartesian_vector = end_cartesian - begin_cartesian;
-            cartesian += cartesian_vector*dot_product;
+
+            if (typeof(From) == uv_vector.GetType())
+            {
+                Vector2 relative_uv = ((Vector2)(object)from) - begin_uv;
+                float dot_product = Vector2.Dot(relative_uv, uv_vector) / uv_vector.sqrMagnitude;
+                to = (To)(object)(cartesian_vector*dot_product + (Vector3)(object)to);
+            }
+            else if (typeof(From) == cartesian_vector.GetType())
+            {
+                Vector3 relative_xyz = ((Vector3)(object)from) - begin_cartesian;
+                float dot_product = Vector3.Dot(relative_xyz, cartesian_vector) / cartesian_vector.sqrMagnitude;
+                to = (To)(object)(uv_vector*dot_product + (Vector2)(object)to);
+            }
         }
 
-        return cartesian;
+        return to;
     }
-
-    private static optional<Mesh> octahedron;
 
     /// <summary>
     /// Inspector - Set up the six mesh corners (vertices) of the octahedron.
@@ -168,6 +160,8 @@ public static class Octahedron
 
         return triangle_array;
     }
+
+    private static optional<Mesh> octahedron;
 }
 
 /*
