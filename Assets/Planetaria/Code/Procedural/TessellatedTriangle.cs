afc92cf8465570b1b0d6b093190910d42653a428
Matthew Russell Downey <matthew.r.downey@gmail.com>
2018-10-10 02:01:53 -0400

    #119: Elegant triangle tessellation

--- Assets/Planetaria/Code/Procedural/TessellatedTriangle.cs
+++ Assets/Planetaria/Code/Procedural/TessellatedTriangle.cs
@@ -6,27 +6,29 @@ public class TessellatedTriangle
     public static Mesh generate(Mesh mesh, int triangle, int level_of_detail, bool triangle_strip = false)
     {
         triangle *= 3;
-        Vector3 a = mesh.vertices[mesh.triangles[triangle+0]].normalized;
-        Vector3 b = mesh.vertices[mesh.triangles[triangle+1]].normalized;
-        Vector3 c = mesh.vertices[mesh.triangles[triangle+2]].normalized;
+        uv_a = mesh.uv[mesh.triangles[triangle+0]].normalized;
+        uv_b = mesh.uv[mesh.triangles[triangle+1]].normalized;
+        uv_c = mesh.uv[mesh.triangles[triangle+2]].normalized;
+        vertex_a = mesh.vertices[mesh.triangles[triangle+0]].normalized;
+        vertex_b = mesh.vertices[mesh.triangles[triangle+1]].normalized;
+        vertex_c = mesh.vertices[mesh.triangles[triangle+2]].normalized;
+        Vector3 ba_edge = vertex_b - vertex_a;
+        Vector3 cb_edge = vertex_c - vertex_b;
+        triangle_plane = Vector3.Cross(ba_edge, cb_edge);
+        area_doubled = triangle_plane.magnitude;
 
         if (level_of_detail < 0)
         {
             Debug.LogError("TessellatedTriangle::generate() must have level_of_detail 0 or greater.");
         }
-        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> interpolators = new List<Vector2>();
         int rows = level_of_detail + 1;
         for (int row = 0; row < rows; row += 1)
         {
             float top_row_interpolator = (row+0)/(float)rows;
             float bottom_row_interpolator = (row+1)/(float)rows;
             //zig-zag through the triangles (back and forth as a single strip)
-            Vector3 zig = (row % 2 == 0 ? b : c);
-            Vector3 zag = (row % 2 == 0 ? c : b);
-            Vector3 top_left = Vector3.Slerp(a, zig, top_row_interpolator);
-            Vector3 bottom_left = Vector3.Slerp(a, zig, bottom_row_interpolator);
-            Vector3 top_right = Vector3.Slerp(a, zag, top_row_interpolator);
-            Vector3 bottom_right = Vector3.Slerp(a, zag, bottom_row_interpolator);
+            bool zig = true;
             TriangleStripDirection next_direction = TriangleStripDirection.TopDown;
             int top_columns = (row+0);
             int bottom_columns = (row+1);
@@ -37,22 +39,26 @@ public class TessellatedTriangle
                 if (next_direction == TriangleStripDirection.TopDown ||
                         next_direction == TriangleStripDirection.TopRight)
                 {
-                    vertices.Add(Vector3.Slerp(top_left, top_right, top_column/(float)top_columns));
+                    float top_column_interpolator = (zig ? top_column : top_columns - top_column);
+                    top_column_interpolator /= top_columns;
+                    interpolators.Add(new Vector2(top_row_interpolator, top_column_interpolator));
                     
                     if (next_direction == TriangleStripDirection.TopDown &&
-                            top_column != 0 /*&& top_column != top_columns*/) // same point has to be added for up to three triangles
+                            top_column != 0) // same point has to be added for up to three triangles
                     {
-                        vertices.Add(Vector3.Slerp(top_left, top_right, top_column/(float)top_columns));
+                        interpolators.Add(new Vector2(top_row_interpolator, top_column_interpolator));
                     }
                 }
                 else // bottom
                 {
-                    vertices.Add(Vector3.Slerp(bottom_left, bottom_right, bottom_column/(float)bottom_columns));
+                    float bottom_column_interpolator = (zig ? bottom_column : bottom_columns - bottom_column);
+                    bottom_column_interpolator /= bottom_columns;
+                    interpolators.Add(new Vector2(bottom_row_interpolator, bottom_column_interpolator));
                     
                     if (next_direction == TriangleStripDirection.BottomUp &&
-                            bottom_column != 0 && bottom_column != bottom_columns) // same point has to be added for up to three triangles
+                            bottom_column != bottom_columns) // same point has to be added for up to three triangles
                     {
-                        vertices.Add(Vector3.Slerp(bottom_left, bottom_right, bottom_column/(float)bottom_columns));
+                        interpolators.Add(new Vector2(bottom_row_interpolator, bottom_column_interpolator));
                     }
                 }
                 
@@ -67,32 +73,82 @@ public class TessellatedTriangle
                 next_direction = (TriangleStripDirection)(((int)next_direction + 1) % 4); // cycle through directions
             }
         }
-        List<Vector3> flipped_vertices = new List<Vector3>();
+        List<Vector2> flipped_interpolators = new List<Vector2>();
         if (triangle_strip == false)
         {
-            for (int triangle_index = 0; triangle_index < vertices.Count; triangle_index += 3)
+            for (int triangle_index = 0; triangle_index < interpolators.Count; triangle_index += 3)
             {
                 // flip triangle vertices
-                Vector3 subvertex_a = vertices[triangle_index+0];
-                Vector3 subvertex_b = vertices[triangle_index+1];
-                Vector3 subvertex_c = vertices[triangle_index+2];
-                Vector3 ba_vector = subvertex_b - subvertex_a;
-                Vector3 cb_vector = subvertex_c - subvertex_b;
+                Vector2 interpolator_1 = interpolators[triangle_index+0];
+                Vector2 interpolator_2 = interpolators[triangle_index+1];
+                Vector2 interpolator_3 = interpolators[triangle_index+2];
+                Vector3 ba_vector = interpolator_2 - interpolator_1;
+                Vector3 cb_vector = interpolator_3 - interpolator_2;
                 Vector3 triangle_direction = Vector3.Cross(ba_vector, cb_vector);
-                bool cull_triangle = Vector3.Dot(triangle_direction, subvertex_a) < 0;
+                bool cull_triangle = Vector3.Dot(triangle_direction, interpolator_1) < 0;
                 if (cull_triangle)
                 {
-                    flipped_vertices.AddRange(new List<Vector3>{ subvertex_c, subvertex_b, subvertex_b }); // flip a and c
+                    flipped_interpolators.AddRange(new List<Vector2>{ interpolator_3, interpolator_2, interpolator_1 }); // flip 1 and 3
                 }
                 else
                 {
-                    flipped_vertices.AddRange(new List<Vector3>{ subvertex_a, subvertex_b, subvertex_c });
+                    flipped_interpolators.AddRange(new List<Vector2>{ interpolator_1, interpolator_2, interpolator_3 });
                 }
             }
         }
+        List<int> triangle_set = new List<int>();
+        List<Vector2> uv_set = new List<Vector2>();
+        List<Vector3> vertex_set = new List<Vector3>();
+        for (int index = 0; index < flipped_interpolators.Count; index += 1)
+        {
+            triangle_set.Add(index);
+            uv_set.Add(interpolated_uv(flipped_interpolators[index]));
+            vertex_set.Add(interpolated_vertex(flipped_interpolators[index]));
+        }
+        Mesh result = new Mesh();
+        result.uv = uv_set.ToArray();
+        result.vertices = vertex_set.ToArray();
+        result.normals = vertex_set.ToArray(); // TODO: verify
+        result.triangles = triangle_set.ToArray();
+        result.RecalculateBounds();
+        return result;
     }
 
+    private static Vector3 interpolated_vertex(Vector2 interpolator)
+    {
+        Vector3 left_rail = Vector3.Slerp(uv_a, vertex_b, interpolator.x);
+        Vector3 right_rail = Vector3.Slerp(uv_a, vertex_c, interpolator.x);
+        return Vector3.Slerp(left_rail, right_rail, interpolator.y);
+    }
+
+    private static Vector2 interpolated_uv(Vector2 interpolator)
+    {
+        Vector3 sphere_position = interpolated_vertex(interpolator);
+        Vector3 triangle_position = Vector3.ProjectOnPlane(sphere_position, triangle_plane);
+        
+        // find Barycentric vectors
+        var vector_a = vertex_a - triangle_position;
+        var vector_b = vertex_b - triangle_position;
+        var vector_c = vertex_c - triangle_position;
+
+        // find Barycentric weights
+        float weight_a = Vector3.Cross(vector_b, vector_c).magnitude / area_doubled;
+        float weight_b = Vector3.Cross(vector_c, vector_a).magnitude / area_doubled;
+        float weight_c = Vector3.Cross(vector_a, vector_b).magnitude / area_doubled;
+
+        // find uv coordinate
+        return uv_a * weight_a + uv_b * weight_b + uv_c * weight_c;
+    }
+    
     private enum TriangleStripDirection { TopDown = 0, BottomRight = 1, BottomUp = 2, TopRight = 3 }
+    private static Vector2 uv_a;
+    private static Vector2 uv_b;
+    private static Vector2 uv_c;
+    private static Vector3 vertex_a;
+    private static Vector3 vertex_b;
+    private static Vector3 vertex_c;
+    private static float area_doubled;
+    private static Vector3 triangle_plane;
 }
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
