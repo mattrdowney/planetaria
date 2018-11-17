a25e2a903087288edbe33ab55102ce49363121d1
Matthew Russell Downey <matthew.r.downey@gmail.com>
2018-11-17 15:03:54 -0500

    PlanetariaLight PointLight algorithm should be very close (hopefully, I don't have to do sampling)

--- Assets/Planetaria/Code/Unity/PlanetariaLight/PointLight.cs
+++ Assets/Planetaria/Code/Unity/PlanetariaLight/PointLight.cs
@@ -34,23 +34,36 @@ namespace Planetaria
             {
                 for (float u = pixel_width/2; u < 1f; u += pixel_width)
                 {
+                    // Look at this hot garbage:
+
                     // Implement https://en.wikibooks.org/wiki/Cg_Programming/Unity/Cookies
                     // With particular notice of "cookieAttenuation = tex2D(_LightTexture0, input.posLight.xy / input.posLight.w + float2(0.5, 0.5)).a"
-                    // w = z / d, where d = 1 (since this is a unit sphere of distance=1), so w = z
-                    // thus UV conversion is: (x,y)/z + (0.5,0.5) = (u,v)
-                    // thus the inverse is ((u,v) - (0.5,0.5))*z = (x,y)
-                    // we do not know z, but we know x^2 + y^2 + z^2 = 1
-                    // derivative knowledge:
+                    // w = z / d
+                    //     where d = 1/tan(FoV(y)/2)
+                    // UV conversion, restated, is: (x,y)/w + (0.5,0.5) = (u,v) [ I should probably compute this via sampling, since that's simpler ] <-- Nevermind this comment, Wolfram Alpha equation solver did it (I was being lazy, I think it would have been easy enough to solve, and I should probably derive the equation as homework)
+                    // we know x^2 + y^2 + z^2 = 1
                     // z^2 = 1 - x^2 - y^2
                     // z = +/-sqrt(1 - x^2 - y^2)
-                    // therefore:
-                    // ((u,v) - (0.5,0.5)) = +/-(x,y)/sqrt(1 - x^2 - y^2)
-                    // breaking this onto two lines:
-                    // u - 0.5 = +/-x/sqrt(1 - x^2 - y^2) [ u >= 0.5 --> x is positive ]
-                    // v - 0.5 = +/-y/sqrt(1 - x^2 - y^2) [ v >= 0.5 --> y is positive ]
-                    // I think there should be another equation based on spotAngle, namely you can find the x/y/z coordinates based on how projection of cucoloris works
+                    // [ u >= 0.5 --> x is positive ]
+                    // [ v >= 0.5 --> y is positive ]
+                    //
+                    // thus:
+                    // tan(FoV/2)*x/sqrt(1 - x^2 - y^2) = u and [I omitted the .5, a happy accident (the more-difficult equation might be required, though)]
+                    // tan(FoV/2)*y/sqrt(1 - x^2 - y^2) = v
+                    // solve for x,y (in Wolfram Alpha)
+                    // returns:
+                    // x = u/sqrt(tan^2(FoV/2) + u^2 + v^2) [you need to multiply by sign(u-.5) and "u" is actually `u - .5`]
+                    // y = v/sqrt(tan^2(FoV/2) + u^2 + v^2)
 
-                    // FIXME: this needs knowledge of the spotlight's field of view
+                    // notably, as FOV approaches 180 degrees, only the centermost pixels are rendered; tan(90) approaches infinity, dividing by ~ infinity approaches 0, which is likely the root of the behavior
+                    // Also, I am essentially doing this:
+                    // use UV coordinates of entire texture:
+                    // convert UV -> XY using above system of equations.
+                    // calculate z = 1 - x^2 - y^2
+                    // calculate Vector3.Angle(Vector3.forward, new Vector3(x,y,z));
+                    // use angle to determine lighting using any function desired (the dot product probably isn't the way to go, I prefer finding the length of the circle at the given radius to compute how spread out (unintense) the light is).
+
+                    // FIXME: this needs knowledge of the spotlight's field of view (this was "d", which I missed)
 
                     UVCoordinates uv = new UVCoordinates(u, v);
                     PolarCoordinates polar = PolarCoordinates.rectangular(uv.data - new Vector2(0.5f, 0.5f)); // Get polar coordinates relative to UV center
@@ -58,7 +71,7 @@ namespace Planetaria
 
                     // we are interested in points of radius [0, 0.5].
                     float angular_distance = Mathf.PI*polar.radius; // this range maps to [0, PI/2]
-                    float dot_product = Mathf.Clamp(Mathf.Sin(angular_distance), Precision.just_above_zero, 1); // Sine range is [-1, +1]
+                    float dot_product = Mathf.Clamp(Mathf.Sin(angular_distance), Precision.just_above_zero, 1); // Sine range is [-1, +1] // dot product uses Cos, right? (I changed this at some point, I think)
                     //float inverse_square = dot_product*dot_product; // Inverse squared lighting (without the inverse!)
                     byte intensity = (byte) Mathf.CeilToInt(dot_product*byte.MaxValue);
                     pixels[pixel] = new Color32(intensity, intensity, intensity, intensity);
